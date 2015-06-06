23b1fbfe523c118e93f47a8f2cd8f3e051b8bd2e
James William George Smith III <devious@nullious.net>
2015-06-05 20:27:40 -0500

    Change newDictionary Template to use an instance variable rather than a Session variable to store label state.

--- client/templates/dictionary/newDictionary.js
+++ client/templates/dictionary/newDictionary.js
@@ -1,39 +1,20 @@
-//Session variables used to keep track of rows. The value is arbitrary, i.e it has no significance
-Session.set("labelRows", [0]);
+ Template.newDictionary.onCreated(function () {
+    // Used to keep track of rows.
+    this.numLabels = new ReactiveVar(0);
+});
 
 Template.newDictionary.helpers({
-    //Is this the last row of labels. Returns true or false
-    'isLastRow': function(index) {
-        var labelRow = Session.get("labelRows");
-
-        var count = labelRow.length;
-
-        if(index == count-1)
-        {
-            return true;
-        } else
-            return false;
-    },
-
-    //Return session variable labelRows
-    'getLabelRows': function(e) {
-        return Session.get("labelRows");
-    },
-
     //getArray is used to create the correct number of rows in the new dictionary page
-    //the returned object of value and index are used to differentiate each HTML element
+    //the returned object of index are used to differentiate each HTML element
     'getArray': function() {
-        var self = this;
+        var labelArray = [];
 
-        //Get labelRows from the session. If it's empty instantiate to empty array
-        self.myArray = Session.get('labelRows') || [];
+        var length = Template.instance().numLabels.get();
+        for (var i = 0; i < length; i++) {
+            labelArray.push({ 'index': i});
+        }
 
-        //Map each element of the array to the function
-        return _.map(self.myArray, function(value,index){
-
-            //Return the objects value and index in an object
-            return {value: value, index: index};
-        });
+        return labelArray;
     }
 });
 
@@ -41,19 +22,16 @@ Template.newDictionary.events({
 
     //Click event to add a row
     'click #increaseRow ': function(event) {
-        //Get the labelRows session variable and add a new element to it. Doesn't matter what
-        var row = Session.get('labelRows');
-        row.push(0);
-
-        //Update the variable
-        Session.set('labelRows', row);
+        // Change the length of the array returned by 'getArray'
+        var num = Template.instance().numLabels;
+        num.set(num.get()+1);
     },
 
     //Click event to delete a row
     'click #deleteRow': function(event) {
         //Get the index of the clicked row
         var index = this.index;
-        
+
         //Remove the approriate row
         $('#row'+index).remove()
     },
@@ -67,60 +45,58 @@ Template.newDictionary.events({
         var validated = true;
 
         //Map function for each element that is required.
-        $(e.target).find('.required').map(function(index, object){            
+        $(e.target).find('.required').map(function(index, object){
             //If any element doesn't have a value, we should fail validation
             if(this.value === '')
                 validated = false;
         })
 
         //If everything passed validation
-        if(validated){
+        if (!validated) {
+            alert("Please fill out all required fields");
+            return;
+        }
 
-            //update data
-            var dictionary = {
-                name: $(e.target).find('[name=title]').val()
+        //update data
+        var dictionary = {
+            name: $(e.target).find('[name=title]').val()
+        };
+
+        //update
+        dictionary._id = Dictionaries.insert(dictionary);
+
+        //Update data for label names
+        var labelNames = [];
+        //Update data for label descriptions
+        var labelDescription = [];
+
+        //Function for each element that has name dynamicVarName
+        $(e.target).find('[name="dynamicVarName[]"]').each(function() {
+            //use this.val() to get the values of each
+            labelNames.push($(this).val());
+        });
+
+        //Function for each element that has name variableType
+        $(e.target).find('[name="variableType[]"]').each(function() {
+            //use this.val() to get the values of each name
+            labelDescription.push($(this).val());
+        });
+
+        //Update
+        for(var i = 0; i < labelNames.length; i++)
+        {
+            //Update data. Grab the positions out of label names/description
+            var adminLabel = {
+                dictionaryID: dictionary._id,
+                label : labelNames[i],
+                description : labelDescription[i]
             };
 
-            //update
-            dictionary._id = Dictionaries.insert(dictionary);
-
-            //Update data for label names
-            var labelNames = [];
-            //Update data for label descriptions
-            var labelDescription = [];
-
-            //Function for each element that has name dynamicVarName
-            $(e.target).find('[name="dynamicVarName[]"]').each(function() {
-                //use this.val() to get the values of each
-                labelNames.push($(this).val());
-            });
-
-            //Function for each element that has name variableType
-            $(e.target).find('[name="variableType[]"]').each(function() {
-                //use this.val() to get the values of each name
-                labelDescription.push($(this).val());
-            });
-
             //Update
-            for(var i = 0; i < labelNames.length; i++)
-            {
-                //Update data. Grab the positions out of label names/description
-                var adminLabel = {
-                    dictionaryID: dictionary._id,
-                    label : labelNames[i],
-                    description : labelDescription[i]
-                };
-
-                //Update
-                Adminlabels.insert(adminLabel);
-            }
-
-            //Route back to the list of dictionaries to see you newly created dictionary
-            Router.go('dictionaries');
+            Adminlabels.insert(adminLabel);
         }
-        //Validation failed
-        else{
-            alert("Please fill out all required fields");
-        }
+
+        //Route back to the list of dictionaries to see you newly created dictionary
+        Router.go('dictionaries');
     }
 });

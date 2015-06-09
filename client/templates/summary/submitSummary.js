e7b91d70b1708dcbc5cc1e7988c8ec66e1d4cb61
James William George Smith III <devious@nullious.net>
2015-06-09 12:20:10 -0500

    Refactor submitSummary Template to submit on button click rather than form submission.

--- client/templates/summary/submitSummary.js
+++ client/templates/summary/submitSummary.js
@@ -1,20 +1,18 @@
-/**
- * Handles the submit form action for submitSummary.html
- *
- * Used when submiting a new summary for a post
- *
- * @param {e} form submit event.
- */
 Template.submitSummary.events({
-    'submit form': function(e) {
-        //Stop the browser from submitting the form.
-        e.preventDefault();
-
+    /**
+    * Handles submit button clicks for submitSummary.html
+    *
+    * Used when submiting a new summary for a post
+    *
+    * @param {e} button click event.
+    */
+    'click input[name=submitSummaryButton]': function(e) {
+        console.log(e);
         //Insert the new summary
         var summary = {
             userID: Meteor.user()._id,
-            postID: $(e.target).find('[name=_id]').val(),
-            text: $(e.target).find('[name=summary]').val(),
+            postID: this._id,
+            text: Template.instance().$('textarea[name=summary]').val(),
             quality_rating : 0,
             numRaters : 0
         };
@@ -22,6 +20,6 @@ Template.submitSummary.events({
         Summaries.insert(summary);
         
         //Redirect to the postpage  
-        Router.go('postPage', {_id: summary.postID});
+        Router.go('postPage', this);
     }
 });

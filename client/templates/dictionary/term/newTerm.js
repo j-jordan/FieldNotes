92eede2a688d472d93171024ee3088fb07469d9f
James William George Smith III <devious@nullious.net>
2015-06-10 19:42:54 -0500

    Refactor newTerm template to avoid using global variables to store template state.

--- client/templates/dictionary/term/newTerm.js
+++ client/templates/dictionary/term/newTerm.js
@@ -1,69 +1,38 @@
-var labelDescription = [];
-var dynamicFields = [];
-
 Template.newTerm.helpers({
-    'getDynamicFields': function(dictId){
-        dynamicFields = Adminlabels.find({dictionaryID: dictId}).fetch();
-        return dynamicFields;
+    'getLabels': function() {
+        return Adminlabels.find({'dictionaryID': this._id});
     }
 });
 
 Template.newTerm.events({
-    //Submit form event for creating a new term
-    'submit form': function(e){
-        //Prevent the default actions
-        e.preventDefault();
-
-        //Data for dictionary page. Used just to pass with router
-        var dictionary = {
-            name: this.name,
-            _id: this._id
-        };
-
-        //Update data for term
+    // Click event for submit button
+    'click button[name=submitButton]': function(e) {
+        // Insert new term
         var term = {
-            term_name: $(e.target).find('[name=term_name]').val(),
+            term_name: Template.instance().$('[name=term_name]').val(),
             dictionaryID: this._id
         };
-
-        //Update terms
         term._id = Terms.insert(term);
 
-        //Update data for definition
-        var defintion = {
+        // Insert new definition
+        Definitions.insert({
             termID: term._id,
             userID: Meteor.user()._id,
-            text: $(e.target).find('[name=definition]').val(),
+            text: Template.instance().$('[name=definition]').val(),
             quality_rating : 0,
             numRaters : 0,
-        };
-
-        //Update definition
-        defintion._id = Definitions.insert(defintion);
-
-        //Reset labelDescription
-        labelDescription = [];
-
-        //For every element with the name labelValue put it's value to labelDescription
-        $(e.target).find('[name="labelValue"]').each(function(){
-            labelDescription.push($(this).val());
         });
 
-        //Update
-        for(var i=0; i<dynamicFields.length ; i++) {
-
-            //Update data
-            var pivot_data = {
+        // For every element with the name labelValue, insert new label value
+        Template.instance().$('[name="labelValue"]').each(function() {
+            Term_label_values.insert({
                 termID: term._id,
-                adminlabelsID: dynamicFields[i]['_id'],
-                value: labelDescription[i]
-            };
+                adminlabelsID: $(this).attr("label_id"),
+                value: $(this).val(),
+            });
+        });
 
-            //Update pivot table
-            Term_label_values.insert(pivot_data);
-        }
-
-         //Route to the dictionary page and send it the data
-        Router.go('dictionaryPage',dictionary);
+         // Route to the dictionary page and send it the data
+        Router.go('dictionaryPage', this);
     }
 });
